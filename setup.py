8812999627a5234c152ca224436e084f29044f35
Ben Epstein <ben.epstein97@gmail.com>
2024-01-18 07:45:13 -0500

    test

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup()
+setup(
+    name='python_project_template',
+    version="0.0.1",
+    packages=find_packages(),
+)
